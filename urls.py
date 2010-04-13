ed5937924f7e2ff529c9034da5a4cb2390214c59
Laura Madsen <lauramadsen@yt.local>
2010-04-13 15:20:54 -0700

    added nav, pages, and content to some pages.

--- urls.py
+++ urls.py
@@ -6,7 +6,12 @@ from thealternativemedicinecabinet.settings import MEDIA_ROOT
 admin.autodiscover()
 
 urlpatterns = patterns('django.views.generic.simple',
-    (r'^$', 'direct_to_template', {'template':'construction.html'}),
+    (r'^$', 'direct_to_template', {'template':'index.html'}),
+    (r'^background/$', 'direct_to_template', {'template':'background.html'}),
+    (r'^speaking/$', 'direct_to_template', {'template':'speaking.html'}),
+    (r'^health_tips/$', 'direct_to_template', {'template':'health_tips.html'}),
+    (r'^books/$', 'direct_to_template', {'template':'books.html'}),
+    (r'^dvds/$', 'direct_to_template', {'template':'dvds.html'}),
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
 
