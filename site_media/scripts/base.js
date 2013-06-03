a23d345297bab56b71ecde7e4f5562a5c88c69d2
lauramadsen <laura.m.madsen@gmail.com>
2013-06-03 17:14:45 +0000

    new book, youtube balancing act video, book info accordions

--- site_media/scripts/base.js
+++ site_media/scripts/base.js
@@ -1,21 +1,19 @@
 $(document).ready(function () {
     $('.toggle_target').hide();
     $('.toggle').click(function (event) {
-        console.log($(event.target).parent());
         if($(event.target).parent().hasClass('closed')) {
-            console.log("open me");
-            $(event.target).innerHtml='Hide Details';
+            $(event.target).text('Hide Details');
             $(event.target).parent().addClass('open');
             $(event.target).parent().removeClass('closed');
-            console.log($(event.target).parent().find('.toggle_target'));
             $(event.target).parent().find('.toggle_target').show();
+            $(event.target).parent().find('.teaser').hide();
         }
         else {
-            console.log("close me");
-            $(event.target).innerHtml='Read More';
+            $(event.target).text('Read More');
             $(event.target).parent().addClass('closed');
             $(event.target).parent().removeClass('open');
             $(event.target).parent().find('.toggle_target').hide();
+            $(event.target).parent().find('.teaser').show();
         }
     })
 
