318d561a09b9c35be3fbee6554e4ef4cd9135a8f
lauramadsen <laura.m.madsen@gmail.com>
2013-12-17 04:50:31 +0000

    start of discount codes

--- site_media/scripts/base.js
+++ site_media/scripts/base.js
@@ -1,20 +1,41 @@
+CODES = ['headstart']
 $(document).ready(function () {
-    $('.toggle_target').hide();
-    $('.toggle').click(function (event) {
-        if($(event.target).parent().hasClass('closed')) {
-            $(event.target).text('Hide Details');
-            $(event.target).parent().addClass('open');
-            $(event.target).parent().removeClass('closed');
-            $(event.target).parent().find('.toggle_target').show();
-            $(event.target).parent().find('.teaser').hide();
-        }
-        else {
-            $(event.target).text('Read More');
-            $(event.target).parent().addClass('closed');
-            $(event.target).parent().removeClass('open');
-            $(event.target).parent().find('.toggle_target').hide();
-            $(event.target).parent().find('.teaser').show();
-        }
-    })
-
-});
+	$('.toggle_target').hide();
+	$('.toggle').click(function (event) {
+		if($(event.target).parent().hasClass('closed')) {
+		    $(event.target).text('Hide Details');
+		    $(event.target).parent().addClass('open');
+		    $(event.target).parent().removeClass('closed');
+		    $(event.target).parent().find('.toggle_target').show();
+		    $(event.target).parent().find('.teaser').hide();
+		}
+		else {
+		    $(event.target).text('Read More');
+		    $(event.target).parent().addClass('closed');
+		    $(event.target).parent().removeClass('open');
+		    $(event.target).parent().find('.toggle_target').hide();
+		    $(event.target).parent().find('.teaser').show();
+		}
+	    });
+	$('.discount_code_target').click( function(event) {
+		$("#discount_code_form").show();
+		$("#discount_code_form").css("top", event.pageY)
+		$("#discount_code_form").css("left", event.pageX)
+	    });
+	$("#use_discount").click( function(event) {
+		event.preventDefault();
+		$("#discount_code_form").hide();
+		var code = $("#discount_code_form input").val();
+		if (CODES.indexOf(code) == -1) {
+		    return
+		}
+		var buttons = $(document).find(".buy_book_button");
+		$.each(buttons, function() {
+			if($(this).hasClass(code)) {
+			    $(this).show();
+			} else {
+			    $(this).hide();
+			}
+		    });
+	    });
+    });
